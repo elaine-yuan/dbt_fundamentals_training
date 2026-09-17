2e02bd7be0d3d767df09ec7b3b6dd238071d8fe5
Elaine Yuan <elaine.yuan@theinformationlab.com>
2026-09-17 18:08:43 +0000

    add audits to compare legacy and refactored code

---
+++ analyses/audits.sql
@@ -0,0 +1,38 @@
+--compare row counts
+
+{% set old_relation = adapter.get_relation(
+      database = target.database,
+      schema = "dbt_eyuan",
+      identifier = "customer_orders_legacy"
+) -%}
+
+{% set dbt_relation = ref('fct_customer_orders') %}
+
+{{ audit_helper.compare_row_counts(
+    a_relation = old_relation,
+    b_relation = dbt_relation
+) }}
+--legacy count 99
+--refactored 99
+
+--compare column values
+
+{% set old_relation = adapter.get_relation(
+      database = target.database,
+      schema = "dbt_eyuan",
+      identifier = "customer_orders_legacy"
+) -%}
+
+{% set dbt_relation = ref('fct_customer_orders') %}
+
+{% if execute %}
+
+{{ audit_helper.compare_all_columns(
+    a_relation = old_relation,
+    b_relation = dbt_relation,
+    primary_key = "order_id"
+) }}
+
+{% endif %}
+
+-- diff col names so not possible to map as-is
